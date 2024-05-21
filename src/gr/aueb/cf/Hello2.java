91a9bd0a79dbe72e6d2e5a299757db6c9085eabf
vagfou <vagfou@protonmail.com>
2024-05-21 20:40:11 +0300

    Hello2 tested and changed

--- src/gr/aueb/cf/Hello2.java
+++ src/gr/aueb/cf/Hello2.java
@@ -3,6 +3,6 @@ package gr.aueb.cf;
 public class Hello2 {
     public static void main(String[] args) {
 
-        System.out.println("Hello again!");
+        System.out.println("Hello again and again!");
     }
 }
